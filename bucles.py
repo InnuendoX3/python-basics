286496fa7dc137e89d8d8abf31fd6e4a416da181
Johan Garzon <johanprograma@hotmail.com>
2021-08-06 15:06:01 +0200

    while loop

--- bucles.py
+++ bucles.py
@@ -1,15 +1,15 @@
-def run(exponent):
-    base = 2
-    result = base ** exponent
-    base_str = str(base)
-    exponent_str = str(exponent)
-    result_str = str(result)
-    if(result <= 1000):
-        print(base_str + " a la " + exponent_str + " = " + result_str)
-        run(exponent + 1)
-    else:
-        print("The end")
+def run():
+    BASE = 2
+    LIMIT = 1000
+    
+    exponent = 0
+    power = BASE**exponent
+    while power < LIMIT:
+        print("The power of " + str(BASE) + " to exponent " + str(exponent) + " is " + str(power))
+        exponent = exponent + 1
+        power = BASE**exponent
+
 
 
 if __name__ == "__main__":
-    run(2)
+    run()
