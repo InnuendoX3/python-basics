ba120e871495bc6b2f86f1236308eb17f09ed9d5
Johan Garzon <johanprograma@hotmail.com>
2021-08-11 11:38:04 +0200

    Tuples

---
+++ tuples.py
@@ -0,0 +1,9 @@
+def run():
+    # Tuples are unchangeable
+    # Define it with parenthesis
+    my_tuple = (True, "A word here", False, 32)
+    print(my_tuple)
+
+
+if __name__ == "__main__":
+    run()
